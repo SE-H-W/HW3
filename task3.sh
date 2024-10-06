2a24fe4fe708899f7522586e05adc8194f5acbe6
Ashwini Ramanuj <100012749+AshwiniR1802@users.noreply.github.com>
2024-10-06 15:32:29 -0400

    Update task3.sh

--- task3.sh
+++ task3.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
-
 # Headers
 echo "PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked" > temp_filtered.csv
 
-# Task a and b: Extract passengers from 2nd class (Pclass = 2)
-gawk -F',' '$3 == 2' titanic.csv | \
-sed  -e 's/female/F/g' -e 's/male/M/g' >> temp_filtered.csv
+# Task a and b: Extract passengers from 2nd class (Pclass = 2) and embarked from Southampton (Embarked = S)
+gawk -F',' '$3 == 2 && ($13 ~ /S/)' titanic.csv | \
+sed -e 's/female/F/g' -e 's/male/M/g' >> temp_filtered.csv
 
 # Print the filtered passengers (Name, Gender, Age) 
 echo "Passengers from 2nd class and female as F and male as M:"
-gawk 'BEGIN {FPAT = "([^,]+)|(\"[^\"]+\")"} {
+gawk 'BEGIN {FPAT = "([^,]+)|(\"[^\"]+\")"} NR > 1 {
     gsub("\"", "", $4); # Remove quotes around Name field
     print "Name: " $4 ", Gender: " $5 ", Age: " $6
 }' temp_filtered.csv
