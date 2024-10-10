9c8addabbc6998746494bf16bd482b6f734654a8
Ashwini Ramanuj <100012749+AshwiniR1802@users.noreply.github.com>
2024-10-10 15:12:51 -0400

    Update task3.sh

--- task3.sh
+++ task3.sh
@@ -3,7 +3,4 @@
 gawk -F, '$3 == 2 && $13 ~ /S/ {print $0}' titanic.csv | sed 's/female/F/g; s/male/M/g' | tee /dev/tty | gawk -F, '$7 != "" { sum += $7; count++ } END {print "Average Age ", sum/count}'
 
 
-#cat titanic.csv | sed 's/female/F/g; s/male/M/g'
-
-#gawk -F, 'NR > 1 && $7 != "" && $3 == 2 && $13 ~ /S/ { sum += $7; count++ } END { if (count > 0) print "Average Age ", sum, count }' titanic.csv
 
