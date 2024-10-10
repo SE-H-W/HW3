ba078eadea332972d756428b8e081a7225802cbe
Ashwini Ramanuj <100012749+AshwiniR1802@users.noreply.github.com>
2024-10-10 14:27:06 -0400

    Update task1.sh

--- task1.sh
+++ task1.sh
@@ -1,1 +1,12 @@
-pkill -f infinite.sh
+#!/bin/bash
+
+# Finding the process ID 
+pid=$(ps aux | grep '[i]nfinite.sh' | awk '{print $2}')
+
+# Checking if found and then terminating it
+if [ -n "$pid" ]; then
+    kill "$pid"
+    echo "Killed process with PID: $pid"
+else
+    echo "No infinite.sh process found."
+fi
